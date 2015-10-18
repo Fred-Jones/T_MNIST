aa515b96c252a7b50b45bc41c5853d4ff09e8314
LaYeka <Fred-Jones@users.noreply.github.com>
2015-10-18 16:44:32 -0600

    Update CSVer.py

--- lib/CSVer.py
+++ lib/CSVer.py
@@ -10,5 +10,4 @@ def read_data(datadir='./data'):
         with open(datadir + '/' + file, 'rwb') as csvfile:
             print 'Loading data {}'.format(file)
             dats.append(csv.reader(csvfile, delimiter=','))
-    print 'Returned tuple of ({0},{1})'.format(files[0], files[1])
     return dats
